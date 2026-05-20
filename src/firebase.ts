d7350eb8f63921bc0d4eb1acdc2272701449035f
Gonzalo <gonzalo@gonzalo-P08CFL>
2026-05-20 19:49:09 -0300

    add redirects

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,17 @@
 import { initializeApp } from "firebase/app"
-
 import { getFirestore } from "firebase/firestore"
-
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyCi-dfco_Cj9XtrYcVfUxdi0i5NOajxSM4",
+  authDomain: "fixture-60083.firebaseapp.com",
+  projectId: "fixture-60083",
+  storageBucket: "fixture-60083.firebasestorage.app",
+  messagingSenderId: "146063947441",
+  appId: "1:146063947441:web:b7c2c777098eaf3f98d490"
 }
 
 const app = initializeApp(firebaseConfig)
 
 export const db = getFirestore(app)
-
 export const storage = getStorage(app)
